1b9a434d9b628f3958588a2381f6a8dcd670bcf5
Bhavin Mec <30741139+FrankBMec@users.noreply.github.com>
2017-09-03 11:05:14 +0530

    Create GridDataModel.cs

---
+++ Models/GridDataModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Responsive_UI_Grid.Models
+{
+    public class GridDataModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string gender { get; set; }
+        public string age { get; set; }
+        public string company { get; set; }
+    }
+}
